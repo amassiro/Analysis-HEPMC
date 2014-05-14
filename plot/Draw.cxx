d6a687d63216a89c09124c6b0e62e7acac30b969
amassiro <massironi.andrea@gmail.com>
2014-05-14 18:05:58 +0200

    new

--- plot/Draw.cxx
+++ plot/Draw.cxx
@@ -1,4 +1,4 @@
-void Draw (std::string var = "hbb_mass", int NBIN = 1000, int MIN = 0, int MAX = 1000, std::string varHR = "") {
+void Draw (std::string var = "hbb_mass", int NBIN = 1000, int MIN = 0, int MAX = 1000, std::string varHR = "", int Energy) {
  
  if (varHR == "") {
   varHR = var;
@@ -26,20 +26,23 @@ void Draw (std::string var = "hbb_mass", int NBIN = 1000, int MIN = 0, int MAX =
  double LUMI = 3000. * 1000.; //---- 1 ab
  
  
- vNameSig.push_back("/tmp/amassiro/vbfhh/Events_2b2w2j/13tev/parton/pp_hh_vbf_SM_13tev_nocuts.root"); vNameSigHR.push_back("HH cv=1.0 c2v=1.0 c3=1.0");
+ std::cout << " Center of mass energy = " << Energy << std::endl;
+ 
+ 
+ vNameSig.push_back("data/signalDelphes/Hvbf_14tev_bbww_CV-1-C2V-1-C3-10.lhe.hepmc.delphes.root"); vNameSigHR.push_back("HH cv=1.0 c2v=1.0 c3=1.0");
  vXsecSig.push_back(0.20088E-04*1.166e-02/10000.); //---- pb
  
- vNameSig.push_back("/tmp/amassiro/vbfhh/Events_2b2w2j/13tev/parton/pp_hh_vbf_BSM_13tev_VBFcuts_CV_p0p5_C2V_p1p0_C3_p1p0.root"); vNameSigHR.push_back("HH cv=0.5 c2v=1.0 c3=1.0");
- vXsecSig.push_back(0.35492E-04*1.166e-02/10000.); //---- pb
- 
- vNameSig.push_back("/tmp/amassiro/vbfhh/Events_2b2w2j/13tev/parton/pp_hh_vbf_BSM_13tev_VBFcuts_CV_p1p0_C2V_p0p0_C3_p1p0.root"); vNameSigHR.push_back("HH cv=1.0 c2v=0.0 c3=1.0");
- vXsecSig.push_back(0.18200E-03*1.166e-02/10000.); //---- pb
- 
- vNameSig.push_back("/tmp/amassiro/vbfhh/Events_2b2w2j/13tev/parton/pp_hh_vbf_BSM_13tev_VBFcuts_CV_p1p0_C2V_p2p0_C3_p1p0.root"); vNameSigHR.push_back("HH cv=1.0 c2v=2.0 c3=1.0");
- vXsecSig.push_back(0.10522E-03*1.166e-02/10000.); //---- pb
- 
- vNameSig.push_back("/tmp/amassiro/vbfhh/Events_2b2w2j/13tev/parton/pp_hh_vbf_BSM_13tev_VBFcuts_CV_p1p5_C2V_p1p0_C3_p1p0.root"); vNameSigHR.push_back("HH cv=1.5 c2v=1.0 c3=1.0");
- vXsecSig.push_back(0.94832E-03*1.166e-02/10000.); //---- pb
+//  vNameSig.push_back("/tmp/amassiro/vbfhh/Events_2b2w2j/13tev/parton/pp_hh_vbf_BSM_13tev_VBFcuts_CV_p0p5_C2V_p1p0_C3_p1p0.root"); vNameSigHR.push_back("HH cv=0.5 c2v=1.0 c3=1.0");
+//  vXsecSig.push_back(0.35492E-04*1.166e-02/10000.); //---- pb
+//  
+//  vNameSig.push_back("/tmp/amassiro/vbfhh/Events_2b2w2j/13tev/parton/pp_hh_vbf_BSM_13tev_VBFcuts_CV_p1p0_C2V_p0p0_C3_p1p0.root"); vNameSigHR.push_back("HH cv=1.0 c2v=0.0 c3=1.0");
+//  vXsecSig.push_back(0.18200E-03*1.166e-02/10000.); //---- pb
+//  
+//  vNameSig.push_back("/tmp/amassiro/vbfhh/Events_2b2w2j/13tev/parton/pp_hh_vbf_BSM_13tev_VBFcuts_CV_p1p0_C2V_p2p0_C3_p1p0.root"); vNameSigHR.push_back("HH cv=1.0 c2v=2.0 c3=1.0");
+//  vXsecSig.push_back(0.10522E-03*1.166e-02/10000.); //---- pb
+//  
+//  vNameSig.push_back("/tmp/amassiro/vbfhh/Events_2b2w2j/13tev/parton/pp_hh_vbf_BSM_13tev_VBFcuts_CV_p1p5_C2V_p1p0_C3_p1p0.root"); vNameSigHR.push_back("HH cv=1.5 c2v=1.0 c3=1.0");
+//  vXsecSig.push_back(0.94832E-03*1.166e-02/10000.); //---- pb
  
  TString name;
  for (int iSig = 0; iSig < nSig; iSig++) {
@@ -49,16 +52,15 @@ void Draw (std::string var = "hbb_mass", int NBIN = 1000, int MIN = 0, int MAX =
  
  //---- background
  
- TFile* f_ttjj = new TFile ("/tmp/amassiro/ttbbjj.root"); //---- 1M events!
- TFile* f_wwbbjj = new TFile ("/tmp/amassiro/wwbbjj.root");
+ TFile* f_ttjj = new TFile ("data/backgroundDelphes/ttbbjj.root"); //---- 1M events!
  
  double ttjj_xsec = 2.0713/1000000.; //---- pb
  // double ttjj_xsec = 2.0715/10000.; //---- pb
- double wwbbjj_xsec = 456.11/10000.; //---- pb
+//  double wwbbjj_xsec = 456.11/10000.; //---- pb
  
  //---- trees
  TTree* t_ttjj = (TTree*) f_ttjj -> Get ("tree");
- TTree* t_wwbbjj = (TTree*) f_wwbbjj -> Get ("tree");
+//  TTree* t_wwbbjj = (TTree*) f_wwbbjj -> Get ("tree");
  for (int iSig = 0; iSig < nSig; iSig++) {
   t_Sig[iSig] = (TTree*) f_Sig[iSig] -> Get ("tree");
  }
@@ -66,7 +68,7 @@ void Draw (std::string var = "hbb_mass", int NBIN = 1000, int MIN = 0, int MAX =
  
  //--------------------------
  TH1F* h_ttjj = new TH1F("ttjj","",NBIN,MIN,MAX);
- TH1F* h_wwbbjj = new TH1F("wwbbjj","",NBIN,MIN,MAX);
+//  TH1F* h_wwbbjj = new TH1F("wwbbjj","",NBIN,MIN,MAX);
  for (int iSig = 0; iSig < nSig; iSig++) {
   name = Form ("hSig_%d",iSig);
   h_Sig[iSig] = new TH1F(name.Data(),vNameSigHR.at(iSig).c_str(),NBIN,MIN,MAX);
@@ -106,9 +108,9 @@ void Draw (std::string var = "hbb_mass", int NBIN = 1000, int MIN = 0, int MAX =
  toDraw = Form ("%s >> ttjj",var.c_str());
  weight = Form ("(%s) * %f",cut.Data(),ttjj_xsec*LUMI);
  t_ttjj->Draw(toDraw.Data(),weight.Data(),"goff");
- toDraw = Form ("%s >> wwbbjj",var.c_str());
- weight = Form ("(%s) * %f",cut.Data(),wwbbjj_xsec*LUMI);
- t_wwbbjj->Draw(toDraw.Data(),weight.Data(),"goff");
+//  toDraw = Form ("%s >> wwbbjj",var.c_str());
+//  weight = Form ("(%s) * %f",cut.Data(),wwbbjj_xsec*LUMI);
+//  t_wwbbjj->Draw(toDraw.Data(),weight.Data(),"goff");
  
  
  
@@ -127,9 +129,9 @@ void Draw (std::string var = "hbb_mass", int NBIN = 1000, int MIN = 0, int MAX =
  h_ttjj->SetLineWidth(2);
  h_ttjj->GetXaxis()->SetTitle(varHR.c_str());
  
- h_wwbbjj->SetMarkerColor(kGreen);
- h_wwbbjj->SetLineColor(kGreen);
- h_wwbbjj->SetLineWidth(2);
+//  h_wwbbjj->SetMarkerColor(kGreen);
+//  h_wwbbjj->SetLineColor(kGreen);
+//  h_wwbbjj->SetLineWidth(2);
  
  TLegend* leg = new TLegend(0.5,0.7,0.9,0.9);
  for (int iSig = 0; iSig < nSig; iSig++) {
@@ -146,18 +148,31 @@ void Draw (std::string var = "hbb_mass", int NBIN = 1000, int MIN = 0, int MAX =
   else h_Sig[iSig]->DrawNormalized("same");
  }
  h_ttjj->DrawNormalized("same");
- h_wwbbjj->DrawNormalized("same");
+//  h_wwbbjj->DrawNormalized("same");
  leg->Draw();
  cn->SetGrid();
  
  //---- lumi scaled
  TCanvas* cnlumi = new TCanvas ("cnlumi","cnlumi",800,600);
  h_ttjj->Draw();
- h_wwbbjj->Draw("same");
+//  h_wwbbjj->Draw("same");
  for (int iSig = 0; iSig < nSig; iSig++) {
   h_Sig[iSig]->Draw("same");
  }
  leg->Draw();
  cnlumi->SetGrid();
  
+ 
+ //--------------
+ //---- dump ----
+ std::cout << std::endl << std::endl << std::endl;
+ for (int iBin=0; iBin<NBIN; iBin++) {
+  for (int iSig = 0; iSig < nSig; iSig++) {
+   std::cout << h_Sig[iSig]->GetBinContent(iBin+1) << " : " ;
+  }
+  std::cout << std::endl;
+ }
+ 
 }
+
+
